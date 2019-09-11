0c32b4b7099c221c2806468d4842fb9f98e376b2
James Volmert <james.volmert@gmail.com>
2019-09-11 16:23:40 -0600

    added map comonent to ui

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,21 +1,49 @@
 <template>
-<div>
-    <h3>hello buddy</h3>
-</div>
-    
+  <div class="map">
+      <div class="google-map" id="map">
+
+      </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Map',
-    data() {
-        return {
-
-        }
+  name: "Map",
+  data() {
+    return {
+        lat: 53,
+        lng: 2
+    };
+  },
+  mounted() {
+    this.makeMap();
+  },
+  methods: {
+    makeMap() {
+        const map = new google.maps.Map(document.getElementById('map'), {
+            center: {
+                lat: this.lat,
+                lng: this.lng,
+            },
+            zoom: 6,
+            maxZoom: 15,
+            minZoom: 3,
+            streetViewControl: false
+        });
     }
-}
+  }
+};
 </script>
 
 <style>
-
+#map {
+    width: 100%;
+    height: 100%;
+    margin: 0 auto;
+    background: #fff;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+}
 </style>   
